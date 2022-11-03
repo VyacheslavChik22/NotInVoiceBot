5a468ee54ff5418f8d82f24adbafac8653964ec1
asyof <asyofka@mail.ru>
2022-11-03 21:36:22 +0300

    add BotInit class

--- src/main/java/service/TelegramBot.java
+++ src/main/java/service/TelegramBot.java
@@ -1,7 +1,57 @@
 package service;
 
+import config.BotConfig;
+import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.Update;
 
 @Service
-public class TelegramBot {
+@Component
+public class TelegramBot extends TelegramLongPollingBot {
+
+    final BotConfig botConfig;
+
+    public TelegramBot(BotConfig botConfig) {
+        this.botConfig = botConfig;
+    }
+
+    @Override
+    public String getBotUsername() {
+        return botConfig.getBotName();
+    }
+
+    @Override
+    public String getBotToken() {
+        return botConfig.getToken();
+    }
+
+    @Override
+    public void onUpdateReceived(Update update) {
+        if (update.hasMessage() && update.getMessage().hasText()) {
+            String messageText = update.getMessage().getText();
+            Long chatId = update.getMessage().getChatId();
+            switch (messageText) {
+
+                case "/start":
+                    responseToStart(chatId, update.getMessage().getChat().getFirstName());
+            }
+
+        }
+    }
+
+    private void responseToStart(Long chatId, String name) {
+        String answer ="Привет, " + name +", ты не прошел в телепроект \"Голос\"но зато ты теперь у нас \"Не в \"Голосе\"\"";
+        sendMessage(chatId,answer);
+    }
+
+    private void sendMessage(Long chatId, String textToSend){
+        SendMessage message = new SendMessage();
+        message.setChatId(String.valueOf(chatId));
+        message.setText(textToSend);
+        execute(message);
+
+
+    }
 }
