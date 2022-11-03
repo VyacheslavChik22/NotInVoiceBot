8c31c0c1d8893ef95358bbe7d57d1131117bf121
asyof <asyofka@mail.ru>
2022-11-03 12:41:50 +0300

    create package config and BotConfig class

--- src/main/java/ru/vyacheslav/NotInVoceBot/NotInVoceBotApplication.java
+++ src/main/java/ru/vyacheslav/NotInVoceBot/NotInVoceBotApplication.java
@@ -1,4 +1,4 @@
-package com.example.notinvocebot;
+package ru.vyacheslav.NotInVoceBot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
---
+++ src/test/java/ru/vyacheslav/NotInVoceBot/NotInVoceBotApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.vyacheslav.NotInVoceBot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotInVoceBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
