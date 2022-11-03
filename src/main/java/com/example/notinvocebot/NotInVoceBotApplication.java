51befca64fdb75f11261d31428f51091054d2998
asyof <asyofka@mail.ru>
2022-11-03 12:11:00 +0300

    Initial commit

---
+++ src/main/java/com/example/notinvocebot/NotInVoceBotApplication.java
@@ -0,0 +1,13 @@
+package com.example.notinvocebot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotInVoceBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NotInVoceBotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/notinvocebot/NotInVoceBotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.notinvocebot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotInVoceBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
