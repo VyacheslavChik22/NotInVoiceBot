d8ce7c3e83425620e1b956c04c31f46a11044572
asyof <asyofka@mail.ru>
2022-11-03 12:59:53 +0300

    add library lombok

--- src/main/java/config/BotConfig.java
+++ src/main/java/config/BotConfig.java
@@ -1,5 +1,20 @@
 package config;
 
+
+import lombok.Data;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+
+@Configuration
+@Data
+@PropertySource("application.properties")
 public class BotConfig {
+
+    @Value("${bot.name}")
+    String botName;
+
+    @Value("${bot.token}")
+    String token;
 
 }
--- src/main/java/ru/vyacheslav/NotInVoceBot/NotInVoiceBotApplication.java
+++ src/main/java/ru/vyacheslav/NotInVoceBot/NotInVoiceBotApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class NotInVoceBotApplication {
+public class NotInVoiceBotApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(NotInVoceBotApplication.class, args);
+		SpringApplication.run(NotInVoiceBotApplication.class, args);
 	}
 
 }
